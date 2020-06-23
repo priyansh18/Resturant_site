c8f950a428a713a2f640f7ae555dc36ed316dd00
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-23 23:19:33 +0530

    Setting up Django project & create Meal app

---
+++ meals/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Meals(models.Model):
+  name = models.CharField(max_length=50)
+  description = models.TextField(max_length=500)
+  people = models.IntegerField()
+  price = models.DecimalField(max_digits=5,decimal_places=2)
+  preparation_time  = models.IntegerField()
+  image= models.ImageField(upload_to='meals/',blank=True)
+
+  def __str__(self):
+    return self.name
