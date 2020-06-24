760b8f9dd427061622b0645c2a9393c2d3fc34b9
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-24 23:08:00 +0530

    Adding Slug Field

--- meals/models.py
+++ meals/models.py
@@ -1,13 +1,27 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
-class Meals(models.Model):
-  name = models.CharField(max_length=50)
-  description = models.TextField(max_length=500)
-  people = models.IntegerField()
-  price = models.DecimalField(max_digits=5,decimal_places=2)
-  preparation_time  = models.IntegerField()
-  image= models.ImageField(upload_to='meals/',blank=True)
 
-  def __str__(self):
-    return self.name
+
+# Slug--It is a way of generating a valid URL, generally using data already obtained. For instance, using the title of an article to generate a URL.
+
+class Meals(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    people = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    preparation_time = models.IntegerField()
+    image = models.ImageField(upload_to='meals/', blank=True)
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug and self.name:
+            self.slug = slugify(self.name)
+        super(Meals, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'meal'
+        verbose_name_plural = 'meals'
+
+    def __str__(self):
+        return self.name
