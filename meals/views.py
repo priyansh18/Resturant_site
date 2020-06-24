5df16aea04a04028d124c415d993b16d1a1303c7
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-24 23:47:28 +0530

    Meals ListView & DetailView

--- meals/views.py
+++ meals/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Meals
 # Create your views here.
+def meal_list(request):
+  meal_list=Meals.objects.all()
+  context={'meal_list':meal_list}
+  return render(request,'meals/list.html',context)
+
+def meal_detail(request,slug):
+  meal_detail=Meals.objects.get(slug=slug)
+  context={'meal_detail':meal_detail}
+  return render(request,'meals/detail.html',context)
--- resturant/urls.py
+++ resturant/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('meals.urls'))
+    path('meals/', include('meals.urls',namespace='meals'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
