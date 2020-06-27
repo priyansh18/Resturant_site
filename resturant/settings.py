8f9b35e4f90b5307f12171d928bdc11c6ff2d789
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-27 12:20:12 +0530

    Adding Reserve Table Form & Use CrispyForms

--- resturant/settings.py
+++ resturant/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'meals',
     'reservation',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,4 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
