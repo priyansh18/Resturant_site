f7cf01726d192f06ede4d604ffe8d151f36dcd68
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-29 22:29:44 +0530

    Added AboutUs Templates

--- aboutus/views.py
+++ aboutus/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Chef, Why_Choose_Us, AboutUs
 # Create your views here.
+
+
+def aboutUs(request):
+    aboutus = AboutUs.objects.last()
+    why_choose_us = Why_Choose_Us.objects.all()
+    chef = Chef.objects.all()
+
+    context = {
+        'aboutus': aboutus,
+        'why_choose_us': why_choose_us,
+        'all_chef': chef,
+    }
+    return render(request, 'aboutus/about.html', context)
--- resturant/urls.py
+++ resturant/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('meals/', include('meals.urls',namespace='meals')),
     path('reserve_table/', include('reservation.urls',namespace='reservation')),
     path('blog/', include('blog.urls',namespace='blog')),
-    # path('aboutus/', include('aboutus.urls',namespace='aboutus'))
+    path('aboutus/', include('aboutus.urls',namespace='aboutus'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
