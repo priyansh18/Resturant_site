24a01d42d73450b3d46a039acef3cdf75c66e1ae
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-30 22:08:45 +0530

    Enabled Searching of Posts

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,21 @@ from .models import Post, Category,Comment
 from taggit.models import Tag
 from .forms import CommentForm
 from django.core.paginator import Paginator
+from django.db.models import Q
 # Create your views here.
 
 
 def post_list(request):
     post_list = Post.objects.all()
+
+    search_query = request.GET.get('q')
+    if search_query:
+        post_list = post_list.filter(
+            Q(title__icontains = search_query)|
+            Q(content__icontains = search_query)|
+            Q(tags__name__icontains = search_query)
+        ).distinct()
+
     paginator = Paginator(post_list,1) #showing 1 post per page
     page = request.GET.get('page')
 
