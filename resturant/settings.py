7e2e83aebe04d3f26dc1a6ec94d5ea3b04f4a71b
Priyansh Singhal <singhalpriyansh58@gmail.com>
2020-09-10 21:42:27 +0530

    Screenshot added

--- resturant/settings.py
+++ resturant/settings.py
@@ -129,13 +129,13 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR,'static')
 ]
 
 
 #Media Files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
